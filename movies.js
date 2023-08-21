c12474d225f4f943bba02db5932e54b4dae29294
TusharRajput07 <tusharrajput2002@gmail.com>
2023-08-21 17:49:52 +0530

    First Commit

---
+++ movies.js
@@ -0,0 +1,138 @@
+const whiplashUrl = 'https://m.media-amazon.com/images/M/MV5BODZjN2E2NDctOWZlMy00MGE0LWFlMmMtYWY0Mzc1NDA2N2VkXkEyXkFqcGdeQXVyNDUzNzExOTE@._V1_.jpg'
+const darkknightUrl = 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg';
+const fightclubUrl = 'https://m.media-amazon.com/images/M/MV5BMjJmYTNkNmItYjYyZC00MGUxLWJhNWMtZDY4Nzc1MDAwMzU5XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1000_.jpg';
+const interstellarUrl = 'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg';
+const theshawshankUrl = 'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg';
+const loganUrl= 'https://m.media-amazon.com/images/M/MV5BYzc5MTU4N2EtYTkyMi00NjdhLTg3NWEtMTY4OTEyMzJhZTAzXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_FMjpg_UX1000_.jpg';
+const inceptionUrl= 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg';
+const goodwillUrl= 'https://m.media-amazon.com/images/M/MV5BOTI0MzcxMTYtZDVkMy00NjY1LTgyMTYtZmUxN2M3NmQ2NWJhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg';
+const thepersuitUrl= 'https://m.media-amazon.com/images/M/MV5BMTQ5NjQ0NDI3NF5BMl5BanBnXkFtZTcwNDI0MjEzMw@@._V1_.jpg';
+const avengersUrl= 'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg';
+
+const breakingbadUrl= 'https://m.media-amazon.com/images/M/MV5BYmQ4YWMxYjUtNjZmYi00MDQ1LWFjMjMtNjA5ZDdiYjdiODU5XkEyXkFqcGdeQXVyMTMzNDExODE5._V1_.jpg';
+const sherlockUrl= 'https://m.media-amazon.com/images/M/MV5BNmJiNDk2MjYtMThkZC00ODU4LThkYmItYjc4ODViYWEyMTg2XkEyXkFqcGdeQXVyMjQ1NjEyNzE@._V1_.jpg';
+const gotUrl= 'https://m.media-amazon.com/images/M/MV5BN2IzYzBiOTQtNGZmMi00NDI5LTgxMzMtN2EzZjA1NjhlOGMxXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_QL75_UX190_CR0,2,190,281_.jpg';
+const theofficeUrl= 'https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_.jpg';
+const thelastUrl= 'https://m.media-amazon.com/images/M/MV5BNGQyYmRkZTgtN2Q1MS00MDllLTg5ZmItNDgyMzczNjVlYWVhXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_FMjpg_UX1000_.jpg';
+const peakyUrl= 'https://m.media-amazon.com/images/M/MV5BZjYzZDgzMmYtYjY5Zi00YTk1LThhMDYtNjFlNzM4MTZhYzgyXkEyXkFqcGdeQXVyMTE5NDQ1MzQ3._V1_FMjpg_UX1000_.jpg';
+const ramayanurl= 'https://m.media-amazon.com/images/M/MV5BODFjYWEyOTktMTU3OC00YTBhLWE4ZmEtNTJiYWUzYTYwMjIzXkEyXkFqcGdeQXVyNjU1NDgwMDg@._V1_FMjpg_UX1000_.jpg';
+const friendsUrl= 'https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg';
+const supernaturalUrl= 'https://m.media-amazon.com/images/M/MV5BNzRmZWJhNjUtY2ZkYy00N2MyLWJmNTktOTAwY2VkODVmOGY3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg';
+const bigbangUrl= 'https://m.media-amazon.com/images/M/MV5BY2FmZTY5YTktOWRlYy00NmIyLWE0ZmQtZDg2YjlmMzczZDZiXkEyXkFqcGdeQXVyNjg4NzAyOTA@._V1_.jpg';
+
+
+
+const movieList = [
+    {name : 'Whiplash', duration : '1h 46m', rating : '8.5', year : '2014', ott : {logo : 'prime.jpg', text : 'Prime Video'}, id : 1, image : whiplashUrl, link : 'https://www.primevideo.com/'},
+    {name : 'The Dark Knight', duration : '2h 32m', rating : '9.0', year : '2008', ott : {logo : 'netflix.png', text : 'Netflix'}, id : 2, image : darkknightUrl, link : 'https://www.netflix.com/in/'},
+    {name : 'Fight Club', duration : '2h 19m', rating : '8.8', year : '1999', ott : {logo : 'prime.jpg', text : 'Prime Video'}, id : 3, image : fightclubUrl, link : 'https://www.primevideo.com/'},
+    {name : 'Interstellar', duration : '2h 49m', rating : '8.7', year : '2014', ott : {logo : 'netflix.png', text : 'Netflix'}, id : 4, image : interstellarUrl, link : 'https://www.netflix.com/in/'},
+    {name : 'The Shawshank Redemption', duration : '2h 22m', rating : '9.3', year : '1994', ott : {logo : 'prime.jpg', text : 'Prime Video'}, id : 5, image : theshawshankUrl, link : 'https://www.primevideo.com/'},
+    {name : 'Logan', duration : '2h 17m', rating : '8.1', year : '2017', ott : {logo : 'disney.jpg', text : 'Disney Plus'}, id : 6, image : loganUrl, link : 'https://www.hotstar.com/in'},
+    {name : 'Inception', duration : '2h 38m', rating : '8.8', year : '2010', ott : {logo : 'netflix.png', text : 'Netflix'}, id : 7, image : inceptionUrl, link : 'https://www.netflix.com/in/'},
+    {name : 'Good Will Hunting', duration : '2h 6m', rating : '8.3', year : '1997', ott : {logo : 'prime.jpg', text : 'Prime Video'}, id : 8, image : goodwillUrl, link : 'https://www.primevideo.com/'},
+    {name : 'The Persuit of Happyness', duration : '1h 57m', rating : '8.0', year : '2006', ott : {logo : 'netflix.png', text : 'Netflix'}, id : 9, image : thepersuitUrl, link : 'https://www.netflix.com/in/'},
+    {name : 'Avengers: Infinity War', duration : '2h 29m', rating : '8.4', year : '2018', ott : {logo : 'disney.jpg', text : 'Disney Plus'}, id : 10, image : avengersUrl, link : 'https://www.hotstar.com/in'},
+    ];
+
+const showsList = [
+    {name : 'Breaking Bad', seasons : '5', rating : '9.5', year : { start : '2008', end  :'2013'},  ott : {logo : 'netflix.png', text : 'Netflix'}, id : 1, image : breakingbadUrl, link : 'https://www.netflix.com/in/'},
+    {name : 'Sherlock', seasons : '2', rating : '9.1', year : { start : '2010', end  :'2017'}, ott : {logo : 'prime.jpg', text : 'Prime Video'}, id : 2, image : sherlockUrl, link : 'https://www.primevideo.com/' },
+    {name : 'Game of Thrones', seasons : '8', rating : '9.2', year : { start : '2011', end  :'2019'}, ott : {logo : 'hbo.jpg', text : 'HBO max'}, id : 3, image : gotUrl, link : 'https://www.max.com/'},
+    {name : 'The Office', seasons : '9', rating : '9.0', year : { start : '2005', end  :'2013'}, ott : {logo : 'netflix.png', text : 'Netflix'}, id : 4, image : theofficeUrl, link : 'https://www.netflix.com/in/'},
+    {name : 'The Last of Us', seasons : '1', rating : '8.8', year : { start : '2023', end  :'2023'}, ott : {logo : 'disney.jpg', text : 'Disney Plus'}, id : 5, image : thelastUrl, link : 'https://www.hotstar.com/in'},
+    {name : 'Peaky Blinders', seasons : '6', rating : '8.8', year : { start : '2013', end  :'2022'}, ott : {logo : 'netflix.png', text : 'Netflix'}, id : 6, image : peakyUrl, link : 'https://www.netflix.com/in/'},
+    {name : 'Ramayan', seasons : '1', rating : '9.0', year : { start : '1987', end  :'1988'}, ott : {logo : 'youtube.jpg', text : 'Youtube'}, id : 7, image : ramayanurl, link : 'https://www.youtube.com/'},
+    {name : 'Friends', seasons : '9', rating : '8.9', year : { start : '1994', end  :'2004'}, ott : {logo : 'netflix.png', text : 'Netflix'}, id : 8, image : friendsUrl, link : 'https://www.netflix.com/in/'},
+    {name : 'Supernatural', seasons : '15', rating : '8.4', year : { start : '2005', end  :'2020'}, ott : {logo : 'prime.jpg', text : 'Prime Video'}, id : 9, image : supernaturalUrl, link : 'https://www.primevideo.com/'},
+    {name : 'The Big Bang Theory', seasons : '12', rating : '8.2', year : { start : '2007', end  :'2019'}, ott : {logo : 'prime.jpg', text : 'Prime Video'}, id : 10, image : bigbangUrl, link : 'https://www.primevideo.com/'},
+];
+
+let currArray = [];
+
+sortByName(movieList.concat(showsList));
+
+
+function renderList(list, isSearch){ 
+    if(!isSearch){
+        currArray = list ;
+    }
+    const progressElement = document.querySelector('.custom-progress-bar');
+    progressElement.classList.add('active');
+    let listHTML = "";
+    for(i=0 ; i<list.length ; i++){
+        const element = list[i];
+        const html = `<div class="box"> 
+
+        <div class="image-box">
+            <img src="${element.image}" height="150px" width="100px" alt="Img">
+        </div> 
+
+        <div class="text-box">
+            <div class="title-box">
+                <div class="id">${i+1}.</div>
+                <div class="title">${element.name}</div>
+            </div>
+
+            <div class="year-box">
+                <div class="year">${element.year.start ? element.year.start + "-" + element.year.end : element.year}</div>
+                <div class="duration">${element.duration ? element.duration : element.seasons + ' ' + 'Seasons'}</div>
+            </div>
+
+            <div class="rating-box">
+                <div class="rating">IMDB: ${element.rating}</div>
+                <div class="ott">
+                    <div>Watch now: </div>
+                    <div class="ott-logo-box" title="${element.ott.text}">
+                        <a href="${element.link}" target="_blank">
+                            <img src="${element.ott.logo}" height="20px" class="ott-logo" alt="${element.ott.text}">
+                        </a>
+                    </div>
+                </div>
+                </div>
+        </div>
+
+    </div>`;
+    listHTML += html;
+    }
+    document.querySelector('.js-container').innerHTML = listHTML;
+    setTimeout(() => {
+        progressElement.classList.remove('active');
+    }, 1000)
+}
+
+function search(){
+    console.log(currArray);
+    const inputElement = document.querySelector('.js-input-search').value;
+    const searchedList = currArray.filter( (item) => {
+        if(item.name.toLowerCase().includes(inputElement.toLowerCase() )){
+            return item;
+        }
+    })
+    renderList(searchedList, true);
+}
+
+function sortByRating() {
+    const sortedList = currArray.sort( (a, b) => {
+        return b.rating - a.rating; 
+    })
+    renderList(sortedList);
+}
+
+function sortByYear() {
+    const sortedList = currArray.sort( (a, b) => {
+        const aStart = a.year.start || a.year
+        const bStart = b.year.start || b.year
+        return aStart - bStart; 
+    })
+    renderList(sortedList);
+}
+
+function sortByName(list) {
+    const sortedList = list.sort( (a, b) => {
+        return a.name.localeCompare(b.name); 
+    })
+    console.log(list, sortedList)
+    renderList(sortedList);
+}
+
